c2a70a48a50476a1f5cb916a5f1996e17ddd63d3
Paul Chen <paulchen647@gmail.com>
2020-10-15 00:32:44 +0000

    Add parameterized queries to students and teachers:

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,21 @@ const pool = new Pool({
 
 const params = process.argv.slice('2');
 console.log(params[0]);
-pool.query(`
-SELECT students.id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${params[0]}%'
-LIMIT ${params[1]};
-`)
+
+ const queryString = `
+ SELECT students.id, students.name as name, cohorts.name as cohort
+ FROM students
+ JOIN cohorts ON students.cohort_id = cohorts.id
+ WHERE cohorts.name LIKE $1
+ LIMIT $2;
+ `;
+ const cohortName = params[0];
+ const limit = params[1];
+
+ const values = [`%${cohortName}%`, limit];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,20 @@ const pool = new Pool({
 
 const params = process.argv.slice('2');
 console.log(params[0]);
-pool.query(`
+const queryString = `
 SELECT teachers.name, cohorts.name AS cohort, count(assistance_requests.*)
 FROM assistance_requests JOIN students ON assistance_requests.student_id = students.id
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`;
+
+const cohort = params[0];
+const values = [cohort];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.name}`);
